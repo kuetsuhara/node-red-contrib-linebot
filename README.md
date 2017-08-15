f80858ef7bd467f0d8d71e60a78e00185e94f87e
上津原 一利 [NEXT] <UetsuharaKazutoshi@next-group.jp>
2017-08-15 16:50:29 +0900

    add reply message

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 
 Linebot for Node-RED
 
-this is line-sdk node.
-require https!!
+* this is line-sdk node.
+* require https!!
+
+## Installation
+npm install node-red-contrib-linebot
--- node.js
+++ node.js
@@ -13,14 +13,13 @@ module.exports = function(RED) {
         // lineクライアント作成
         const client = new line.Client(lineconfig);
         node.on('input', function(msg) {
-            
             // get line event
             const line_event = msg.payload.events[0];
             // get message
             const received_msg = line_event.message.text;
 
             // create reply
-            var massage = { type: 'text', text: received_msg };
+            var massage = { type: 'text', text: config.replyMessage };
             client.replyMessage(line_event.replyToken, massage);
 
             var values = [client, line_event];
